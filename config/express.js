3bfb760c951be6222a50993d60dce98b8333994d
shaun <shaun965@hotmail.com>
2015-11-29 20:41:41 -0800

    Added mongodb :)

--- config/express.js
+++ config/express.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require('express'),
+    config = require('./config'),
+    bodyParser = require('body-parser');
+
+
 module.exports = function() {
     var app = express();
+
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+
+    app.use(bodyParser.json());
+
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
     app.use(express.static('./public'));
+
     require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/users.server.routes.js')(app);
+    
     return app;
 };
